608c3a0a48ddb41a7298c7c5174c48875513255e
David <dave.bartsch@icloud.com>
2022-06-06 14:18:52 -0600

    This should be on branch client_reg

--- JoinYou/App.js
+++ JoinYou/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hopefully this updates!</Text>
+      <Text>Hopefully this updates!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
