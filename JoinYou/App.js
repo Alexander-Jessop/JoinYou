d888337ad60815542585cc0564dfdffcff969a47
Zach Odio <zachariaodio@gmail.com>
2022-06-06 14:32:19 -0600

    test

--- JoinYou/App.js
+++ JoinYou/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hopefully this updates!!!</Text>
+      <Text>I am chenging to client branch!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
