116e28d41e3c4560bff6f1b9948d7945c789d68b
David <dave.bartsch@icloud.com>
2022-06-08 19:29:46 -0600

    removed two !

--- JoinYou/App.js
+++ JoinYou/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>I am chenging to client branch!!!</Text>
+      <Text>I am chenging to client branch!</Text>
       <StatusBar style="auto" />
     </View>
   );
