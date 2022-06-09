6397fd8365cf3aabb02e505f9c4a8e7f97aa3d11
Zach Odio <zachariaodio@gmail.com>
2022-06-08 19:23:01 -0600

    username taken off

--- JoinYou/App.js
+++ JoinYou/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>I am chenging to client branch!!!</Text>
+      <Text>Registrations</Text>
       <StatusBar style="auto" />
     </View>
   );
