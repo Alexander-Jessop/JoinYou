8b8d1673793004ca15b15dda4b6f94ac71170e96
David <dave.bartsch@icloud.com>
2022-06-08 19:16:40 -0600

    added TimezonePicker

---
+++ JoinYou/components/ui/TimezonePicker.jsx
@@ -0,0 +1,40 @@
+import { Button, View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import timezones from "./timezones.json";
+import { Picker } from "@react-native-picker/picker";
+
+const TimezonePicker = () => {
+  const [selectedTimezone, setSelectedTimezone] = useState();
+
+  return (
+    <View>
+      <Text styles={{ justifyContent: "center" }}>
+        Select your timezone:
+      </Text>
+
+      <Picker
+        selectedValue={selectedTimezone}
+        onValueChange={(itemValue, itemIndex) =>
+          setSelectedTimezone(itemValue)
+        }
+        style={{ height: 30, width: 350 }}
+        itemStyle={{ fontSize: 12 }}
+      >
+        {timezones.map((timezone) => (
+          <Picker.Item label={timezone.text} value={timezone.utc[0]} />
+        ))}
+      </Picker>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default TimezonePicker;
