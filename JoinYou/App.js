529958bde791f49fa6be5d124218a56993aa4748
David <dave.bartsch@icloud.com>
2022-06-10 13:30:11 -0600

    Moved contents of App.js to RestOfApp.jsx

--- JoinYou/App.js
+++ JoinYou/App.js
@@ -1,27 +1,10 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import InitialScreen from "./screens/InitialScreen";
-import ClientRegInfo from "./screens/ClientRegInfo";
-import ClientRegTags from "./screens/ClientRegTags";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
-const Stack = createNativeStackNavigator();
+import RestOfApp from "./components/RestOfApp";
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Initial"
-          component={InitialScreen}
-          options={{ title: "Welcome" }}
-        />
-        <Stack.Screen name="Register" component={ClientRegInfo} />
-        <Stack.Screen name="Tags" component={ClientRegTags} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <RestOfApp />;
 }
 
 const styles = StyleSheet.create({
---
+++ JoinYou/components/RestOfApp.jsx
@@ -0,0 +1,23 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginPage from "../screens/LoginPage";
+
+const Stack = createNativeStackNavigator();
+
+const RestOfApp = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={LoginPage}
+          options={{ title: "Welcome" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default RestOfApp;
