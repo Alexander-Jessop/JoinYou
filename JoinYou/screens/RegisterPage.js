7d14670dfbf9be9d38ec5b563c0eebb0e1a7e50b
David <dave.bartsch@icloud.com>
2022-06-11 12:54:24 -0600

    Added onContinueHandler to RegisterPage button

--- JoinYou/screens/RegisterPage.js
+++ JoinYou/screens/RegisterPage.js
@@ -1,12 +1,26 @@
 import React, { useState } from "react";
 import { Button, Text, TextInput, View, StyleSheet } from "react-native";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 const RegisterPage = ({ navigation }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const createUserHandler = () => {
+  const auth = getAuth();
+
+  const onContinueHandler = () => {
     navigation.navigate("Tags");
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+      });
   };
 
   return (
@@ -31,7 +45,7 @@ const RegisterPage = ({ navigation }) => {
         ></TextInput>
       </View>
 
-      <Button title="Continue" onPress={createUserHandler} />
+      <Button title="Continue" onPress={onContinueHandler} />
     </View>
   );
 };
