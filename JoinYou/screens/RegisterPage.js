7bbc2fa6c2c7a84c835ccfbfa91dae604df67cd0
David <dave.bartsch@icloud.com>
2022-06-11 12:42:28 -0600

    Added forms to Register Page

--- JoinYou/screens/RegisterPage.js
+++ JoinYou/screens/RegisterPage.js
@@ -1,14 +1,56 @@
-import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { Button, Text, TextInput, View, StyleSheet } from "react-native";
 
-const RegisterPage = () => {
+const RegisterPage = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const createUserHandler = () => {
+    navigation.navigate("Tags");
+  };
+
   return (
-    <View>
-      <Text>Register Page</Text>
+    <View style={styles.container}>
+      <View>
+        <Text>Enter your email address:</Text>
+        <TextInput
+          value={email}
+          onChangeText={(enteredEmail) => setEmail(enteredEmail)}
+          placeholder="Email"
+          style={styles.textBoxes}
+        ></TextInput>
+      </View>
+
+      <View>
+        <Text>Enter your password:</Text>
+        <TextInput
+          value={password}
+          onChangeText={(enteredPassword) => setPassword(enteredPassword)}
+          placeholder="Password"
+          style={styles.textBoxes}
+        ></TextInput>
+      </View>
+
+      <Button title="Continue" onPress={createUserHandler} />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textBoxes: {
+    width: "90%",
+    fontSize: 18,
+    padding: 12,
+    borderColor: "gray",
+    borderWidth: 0.2,
+    borderRadius: 10,
+  },
+});
 
 export default RegisterPage;
