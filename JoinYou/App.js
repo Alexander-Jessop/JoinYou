9a907becf849cacf0d4f8ce281f2ae92370f5855
napizar8 <napizar8@gmail.com>
2022-06-13 17:10:58 -0600

    added name

--- JoinYou/App.js
+++ JoinYou/App.js
@@ -85,7 +85,7 @@ export default function App() {
       <AuthProvider>
         <StatusBar style="light" />
         <RestOfApp />
-        high I am Zach
+        high I am Zach hi I'm nathaly
       </AuthProvider>
     </FirebaseProvider>
   );
