ce03663a4053c34ac005187bc625e4d2a9887e74
David <dave.bartsch@icloud.com>
2022-06-13 14:53:24 -0600

    removed unnecessary import

--- JoinYou/App.js
+++ JoinYou/App.js
@@ -1,4 +1,3 @@
-import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import RestOfApp from "./src/RestOfApp";
