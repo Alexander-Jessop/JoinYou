ec05815f9b45c89bdf9fb1c29edb9634b81f0bdf
Zach Odio <zachariaodio@gmail.com>
2022-06-13 17:06:55 -0600

    test

--- JoinYou/App.js
+++ JoinYou/App.js
@@ -85,7 +85,8 @@ export default function App() {
       <AuthProvider>
         <StatusBar style="light" />
         <RestOfApp />
+        high I am Zach
       </AuthProvider>
     </FirebaseProvider>
   );
-}
+  
