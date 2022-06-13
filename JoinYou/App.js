c411d55ac17e1363b4ef9d52c64a7e1d394bed34
Alexander Jessop <alexanderjessop@protonmail.com>
2022-06-13 17:18:07 -0600

    I beat you Nat.

--- JoinYou/App.js
+++ JoinYou/App.js
@@ -85,8 +85,6 @@ export default function App() {
       <AuthProvider>
         <StatusBar style="light" />
         <RestOfApp />
-        high I am Zach hi I'm nathaly
       </AuthProvider>
     </FirebaseProvider>
   );
-  
