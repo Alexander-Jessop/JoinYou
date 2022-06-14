77bec02395a7d9efb872c84552504d67e929c0bb
David <dave.bartsch@icloud.com>
2022-06-14 10:32:47 -0600

    moved client_reg screens to a new folder

---
+++ JoinYou/Screens/client-reg-screens/TagsPage.js
@@ -0,0 +1,34 @@
+import React, { useContext, useState } from "react";
+import { Text, View, StyleSheet, ScrollView, Button } from "react-native";
+import { Checkbox } from "react-native-paper";
+import { AuthContext } from "../src/AuthProvider";
+import { useNavigation } from "@react-navigation/native";
+import TagsForm from "../../components/TagsForm";
+
+const TagsPage = () => {
+  return (
+    <ScrollView>
+      <TagsForm />
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  checkboxContainer: {
+    flexDirection: "row",
+    marginBottom: 20,
+  },
+  checkbox: {
+    alignSelf: "center",
+  },
+  label: {
+    margin: 8,
+  },
+});
+
+export default TagsPage;
