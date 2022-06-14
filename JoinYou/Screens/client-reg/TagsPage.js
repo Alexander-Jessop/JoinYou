6524c0b39df812169569c46f55dee41bd44eda64
David <dave.bartsch@icloud.com>
2022-06-14 11:07:55 -0600

    removed unnecessary imports

--- JoinYou/Screens/client-reg/TagsPage.js
+++ JoinYou/Screens/client-reg/TagsPage.js
@@ -1,8 +1,5 @@
 import React, { useContext, useState } from "react";
 import { Text, View, StyleSheet, ScrollView, Button } from "react-native";
-import { Checkbox } from "react-native-paper";
-import { AuthContext } from "../src/AuthProvider";
-import { useNavigation } from "@react-navigation/native";
 import TagsForm from "../../components/registration/TagsForm";
 
 const TagsPage = () => {
