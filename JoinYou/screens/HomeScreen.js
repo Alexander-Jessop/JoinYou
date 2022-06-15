f8576e5a94b0546bd73af76161027f4fdc0f6135
David <dave.bartsch@icloud.com>
2022-06-15 16:43:42 -0600

    Made feed based on categories

--- JoinYou/screens/HomeScreen.js
+++ JoinYou/screens/HomeScreen.js
@@ -4,6 +4,7 @@ import { AuthContext } from "../src/AuthProvider";
 import { useNavigation } from "@react-navigation/native";
 import TagsForm from "../components/registration/TagsForm";
 import ExpertList from "../components/feed/ExpertList";
+import CategoryList from "../components/feed/CategoryList";
 
 const HomeScreen = () => {
   const authContext = useContext(AuthContext);
@@ -23,6 +24,7 @@ const HomeScreen = () => {
 
       <Text>THIS IS THE HOME SCREEN</Text>
       <ExpertList />
+      <Button title="See all categories" />
     </ScrollView>
   );
 };
