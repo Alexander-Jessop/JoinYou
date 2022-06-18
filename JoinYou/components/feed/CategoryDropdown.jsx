6816f9d9b2562474966137235051f6315329146f
David <dave.bartsch@icloud.com>
2022-06-17 18:30:08 -0600

    added comments to old method of data retrieval

--- JoinYou/components/feed/CategoryDropdown.jsx
+++ JoinYou/components/feed/CategoryDropdown.jsx
@@ -23,35 +23,40 @@ const CategoryDropdown = (props) => {
   const handlePress = () => setExpanded(!expanded);
 
   useEffect(() => {
+    // //Query snapshot that returns all users in the firestore db.
     // let collectionRef = collection(db, "users");
     // let queryRef = query(collectionRef, orderBy("displayName"));
     // const unsubscribe = onSnapshot(queryRef, (querySnap) => {
     //   if (querySnap.empty) {
     //     console.log("No docs found");
     //   } else {
+    //     //Map over the query results and store them in usersData.
     //     let usersData = querySnap.docs.map((doc) => doc.data());
 
+    //     //Filter only the experts from usersData.
     //     let expertsData = usersData?.filter((user) => {
     //       return user.isExpert;
     //     });
 
+    //     //Filter the list of experts by category.
     //     let filteredByCategory = expertsData?.filter((expert) => {
     //       return expert.interests.includes(selectedCategory);
     //     });
 
+    //     //Set expertsByCategory state to the filtered list.
     //     setExpertsByCategory(filteredByCategory);
     //   }
     // });
     // return unsubscribe;
 
-    //Get multiple documents from a collection with a filter
+    //Get multiple documents from a collection with a filter.
     //https://firebase.google.com/docs/firestore/query-data/get-data#get_multiple_documents_from_a_collection
     const getData = async () => {
       const q = query(collection(db, "users"), where("isExpert", "==", true));
       const querySnapshot = await getDocs(q);
       const expertArray = querySnapshot.docs.map((doc) => doc.data());
 
-      //Second filter. Filtering by selected category
+      //Second filter. Filtering by selected category.
       const filteredByCategory = expertArray.filter((expert) => {
         return expert.interests.includes(selectedCategory);
       });
