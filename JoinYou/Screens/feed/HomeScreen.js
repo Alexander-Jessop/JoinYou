85c7b4214ed0c24c6d93ec52206991e11962f7b4
David <dave.bartsch@icloud.com>
2022-06-20 12:15:21 -0600

    removed unnecessary HomeList import

--- JoinYou/Screens/feed/HomeScreen.js
+++ JoinYou/Screens/feed/HomeScreen.js
@@ -3,7 +3,6 @@ import { Button, Text, View, StyleSheet, ScrollView } from "react-native";
 import { AuthContext } from "../../src/AuthProvider";
 import { useNavigation } from "@react-navigation/native";
 import MainList from "../../components/feed/MainList";
-import HomeList from "../../components/feed/HomeList";
 
 const HomeScreen = ({ navigation }) => {
   const authContext = useContext(AuthContext);
