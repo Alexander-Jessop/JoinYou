3f3891d7e1d1b7be7b67e2477caa11f75441e988
David <dave.bartsch@icloud.com>
2022-06-20 13:23:03 -0600

    DataList re-upload

---
+++ JoinYou/components/feed/DataList.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Text, View, StyleSheet, ScrollView } from "react-native";
+import { Avatar, Button, Card, Title, Paragraph } from "react-native-paper";
+
+const DataList = (props) => {
+  return (
+    <ScrollView>
+      {props.filteredData.map((expert) => {
+        return (
+          <Card title={expert.displayName} key={expert.uid} style={styles.card}>
+            <Card.Content>
+              <Title>Name: {expert.displayName}</Title>
+
+              <Paragraph>Expertise in: {expert.interests.join(", ")}</Paragraph>
+
+              <Text>{"\n"}</Text>
+            </Card.Content>
+          </Card>
+        );
+      })}
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    padding: 30,
+  },
+});
+
+export default DataList;
