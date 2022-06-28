be20a3849750be863ac24fdfe77122a28cc025ce
napizar8 <napizar8@gmail.com>
2022-06-28 12:44:48 -0600

    added firebase in server folder

---
+++ server/functions/index.js
@@ -0,0 +1,16 @@
+import functions from "firebase-functions";
+import { initializeApp } from "firebase-admin/app";
+
+
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", { structuredData: true });
+//   response.send("Hello from Firebase!");
+// });
+
+
+export const timeslotparse = functions.https.onRequest((req, res) => {
+    
+})
+
+
+
