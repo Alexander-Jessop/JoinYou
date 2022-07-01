9d47ba22b8909823a916448e6ebb83302bf48e39
Alexander Jessop <alexanderjessop@protonmail.com>
2022-07-01 15:53:55 -0600

    changed firebase version

--- JoinYou/babel.config.js
+++ JoinYou/babel.config.js
@@ -1,12 +1,6 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      "babel-preset-expo",
-      // We add the following code
-      "module:metro-react-native-babel-preset",
-      "module:react-native-dotenv",
-      // End of added code
-    ],
+    presets: ["babel-preset-expo"],
   };
 };
