50841ccd7b94ad027c659d91cfc29186ac67f82a
napizar8 <napizar8@gmail.com>
2022-07-06 13:55:18 -0600

    wrote gregs code from bug brigade

--- JoinYou/src/CloudFunctions.js
+++ JoinYou/src/CloudFunctions.js
@@ -1,34 +1,66 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, { useContext, useState } from 'react'
-import { Button } from 'react-native-paper';
-import { FirebaseContext } from './FirebaseProvider';
-import { httpsCallable } from 'firebase/functions';
+import { StyleSheet, Text, View } from "react-native";
+import React, { useContext, useState } from "react";
+import { Button } from "react-native-paper";
+import { FirebaseContext } from "./FirebaseProvider";
+import { httpsCallable } from "firebase/functions";
 
 const CloudFunctions = () => {
-    const fbContext = useContext(FirebaseContext);
-    const cloudFuncs = fbContext.cloudFuncs;
-    const [count, setCount] = useState("n/a");
-  
-    const getUsersCount = async () => {
-      try {
-        const getNumberOfUsers = httpsCallable(cloudFuncs, "getNumberOfUsers");
-        const result = await getNumberOfUsers();
-        const data = result.data;
-        console.log(`data is:`, data);
-        setCount(data.numUsers);
-      } catch(ex) {
-        console.log('yikes! ', ex.message);
-        throw ex;
-      }
+  const fbContext = useContext(FirebaseContext);
+  const cloudFuncs = fbContext.cloudFuncs;
+  const db = fbContext.db;
+  const [count, setCount] = useState("n/a");
+  const [rsponse, setResponse] = useState();
+
+  const getUsersCount = async () => {
+    try {
+      const getNumberOfUsers = httpsCallable(cloudFuncs, "getNumberOfUsers");
+      const result = await getNumberOfUsers();
+      const data = result.data;
+      console.log(`data is:`, data);
+      setCount(data.numUsers);
+    } catch (ex) {
+      console.log("yikes! ", ex.message);
+      throw ex;
     }
+  };
+
+  const addRandoUser = async () => {
+    try {
+      console.log("Function to add a new user to firestore database");
+      const addUser = httpsCallable(cloudFuncs, "addRandoUser");
+      const result = await addUser();
+      const data = result.data;
+      console.log("New user added", data);
+    } catch (ex) {
+      console.log("yikes! ", ex.message);
+      throw ex;
+    }
+  };
+  const initializeInfl = async () => {
+    const myInitializeFunc = httpsCallable(
+      cloudFuncs,
+      "initializeNewInfluencer"
+    );
+    const result = await myInitializeFunc({
+      startTime: "11:00",
+      endTime: "14:00",
+      meetingLength: 15,
+    });
+    const data = result.data;
+    setResponse(data);
+  };
+
   return (
-    <View style = {styles.container}>
+    <View style={styles.container}>
       <Text>CloudFunctions {count} </Text>
-      <Button onPress = {getUsersCount}> click it</Button>
+      <Button onPress={getUsersCount}> click it</Button>
+      <Text>Number of users {count} </Text>
+      <Button onPress={addRandoUser}> Add a user</Button>
+      <Text>Response from the function {response} </Text>
     </View>
-  )
-}
+  );
+};
 
-export default CloudFunctions
+export default CloudFunctions;
 
-const styles = StyleSheet.create({container:{alignItems: "center"}})
+const styles = StyleSheet.create({ container: { alignItems: "center" } });
--- server/functions/index.js
+++ server/functions/index.js
@@ -1,30 +1,100 @@
 import functions from "firebase-functions";
 import { initializeApp } from "firebase-admin/app";
-// import { collection, doc, setDoc } from "firebase/firestore"; 
-import { getFirestore } from 'firebase-admin/firestore';
+import { getFirestore, addDoc, collection } from 'firebase-admin/firestore';
+// import admin from 'firebase-admin';
 
 const app = initializeApp();  // uses current Firebase project’s config
 const firestore = getFirestore(app);
 
-const inflSetBlocks = {
-  fromTime:  "9:00",
-  toTime : "11:00",
-  influencerAvailabilityDate: "2022-06-29",
-}
+// //  first version
+// export const getNumberOfUsers = functions.https.onCall(
+//   async (data, context) => {
+//     try {
+//       let userCollRef = firestore.collection('users');
+//       let querySnap = await userCollRef.get();
+//       // functions.logger.log(`request from: ${context.auth.token.email}`);
+//       functions.logger.log(`num of users: ${querySnap.size}`);
+//       return { numUsers: querySnap.size };
+//     } catch (ex) {
+//       functions.logger.info(`ERROR: ${ex.message}`);
+//       throw ex;
+// }})
 
 
-export const getNumberOfUsers = functions.https.onCall(
-  async (data, context) => {
-    try {
-      let userCollRef = firestore.collection('users');
-      let querySnap = await userCollRef.get();
-      // functions.logger.log(`request from: ${context.auth.token.email}`);
-      functions.logger.log(`num of users: ${querySnap.size}`);
-      return { numUsers: querySnap.size };
-    } catch (ex) {
-      functions.logger.info(`ERROR: ${ex.message}`);
-      throw ex;
-}})
+// //  Add a user to the database
+// export const addRandoUser = functions.https.onCall(
+//   async (data, context) => {
+//     try{
+//       let userCollRef = firestore.collection('users');
+//       const docRef = await addDoc(collection(userCollRef, "users"), {
+//         displayName: "FakeName" + (count+1),
+//         email: "FakeEmail"+(count+1)
+//       });
+//       console.log("Document writen with ID:", docRef.id)
+//       return docRef
+//     }catch(ex) {
+//       console.log('yikes! ', ex.message);
+//       throw ex;
+//     }
+//     }
+// )
 
 
 //  Agregaritng counts
+     export const fsUsersOnCreate = functions.firestore
+      .document('users/{docId}')
+      .onCreate(async (docSnap, context) => {
+        try {
+          const newUser = docSnap.data();
+          functions.logger.log(`new user! ${newUser.email}`);
+          const aggDocRef = firestore.doc("stats/users");
+          return aggDocRef.update(
+            {count: admin.firestore.FieldValue.increment(1)});
+          } catch (ex) { 
+            functions.logger.info(`ERROR: ${ex.message}`);
+          throw ex;
+        }});
+
+        // //  efficiently get the users
+        // export const getNumberOfHeroes2 = functions.https.onCall(async(data, context) =>
+        // {
+        // try {
+        //   let aggDocRef = firestore.doc("stats/users");
+        //   let docSnap = await aggDocRef.get(aggDocRef);
+        //   let theCount = docSnap.data().count;
+        //   functions.logger.log(`request from: ${context.auth.token.email}`);
+        //   functions.logger.log(`num of users2: ${theCount}`);
+        //   return { numUsers: theCount };
+        // } catch (ex) {
+        //   functions.logger.info(`ERROR: ${ex.message}`);
+        //   throw ex;
+        // }});
+
+//  Do an onCall
+export const initializeNewUnfluencer = functions.https.conCall(
+  async (data, context) => {
+    try {
+      let theUid = context.auth.uid;
+      let theEmail = context.auth.token.email;
+console.log(`the UID is: `, theUid);
+
+console.log(`the data is:`, data);
+let theStartTime = data.startTime;
+let theMeetingLength = data.meetingLength;
+let theNumMeetings = data.numMeetings;
+
+for (let i = 0; i < themeetings; i++) {
+  let meetingCollRef = firestore.collection('meetings');
+  let docSnap = await meetingCollRef.add({
+    uid: theUid,
+    email: theEmail,
+startTime: theStartTime + i * theMeetingLength /60,
+  });
+  console.log(`the new docID is:`, docSnap.id)
+}
+    }catch (ex){
+      functions.logger.info(`ERROR: ${ex.message}`);
+      throw ex;
+    }
+  }
+)
