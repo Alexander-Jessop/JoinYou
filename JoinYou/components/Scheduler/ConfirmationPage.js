8f8e0fe07821e3cfcaddf43b87b2350ee8ffc16f
David <dave.bartsch@icloud.com>
2022-07-07 14:32:55 -0600

    Added TextInput for description

--- JoinYou/components/Scheduler/ConfirmationPage.js
+++ JoinYou/components/Scheduler/ConfirmationPage.js
@@ -1,28 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, StyleSheet } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-import { Button } from "react-native-paper";
+import { Button, Card, Text, TextInput, Title } from "react-native-paper";
 
-const ConfirmationPage = () => {
+const ConfirmationPage = (props) => {
   const navigation = useNavigation();
+  const { route } = props;
+  const selectedDate = route.params.selectedDate;
+  const selectedSlot = route.params.selectedSlot;
+
+  const [description, setDescription] = useState("");
 
   return (
     <View>
+      <Card>
+        <Card.Content>
+          <Title>
+            Booking an appointment for: {selectedSlot}, {selectedDate}
+          </Title>
+
+          <TextInput
+            label="Describe your appointment (200 characters max)"
+            multiline={true}
+            maxLength={200}
+            selectionColor="#007f5f"
+            underlineColor="#007f5f"
+            activeOutlineColor="#007f5f"
+            activeUnderlineColor="#007f5f"
+            onChangeText={(e) => setDescription(e)}
+          />
+
+          <Button
+            color="#007F5F"
+            onPress={() => {
+              navigation.navigate("Video");
+            }}
+          >
+            Take a Video
+          </Button>
+          <Button
+            color="#007F5F"
+            onPress={() => {
+              navigation.navigate("Photo");
+            }}
+          >
+            Take a Photo
+          </Button>
+        </Card.Content>
+      </Card>
+
       <Button
-        onPress={() => {
-          navigation.navigate("Video");
-        }}
-      >
-        Take Video
-      </Button>
-      <Button
-        onPress={() => {
-          navigation.navigate("Photo");
-        }}
-      >
-        Take Photo
-      </Button>
-      <Button
+        color="#007F5F"
+        mode="contained"
         onPress={() => {
           navigation.navigate("Payment");
         }}
@@ -33,6 +62,10 @@ const ConfirmationPage = () => {
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  button: {
+    color: "#007f5f",
+  },
+});
 
 export default ConfirmationPage;
